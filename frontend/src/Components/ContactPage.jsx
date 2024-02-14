c302bea570a42602838e3e3ed86f020764430850
Richard Danon-Woedey <richard2004danon@gmail.com>
2024-02-13 19:15:27 -0500

    Fixed contact page

--- frontend/src/Components/ContactPage.jsx
+++ frontend/src/Components/ContactPage.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import '../App.css';
-
-import OfficeImage from '../Images/office.jpg'; // Make sure the path to your image is correct
+import OfficeImage from '../Images/office.jpg';
 
 const ContactPage = () => {
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
   const linkdnIconSVG = () => {
     return (
       <svg width="35" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -28,6 +33,65 @@ const ContactPage = () => {
     )
   }
 
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('https://formspree.io/f/xdoqgevg', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email,
+          subject,
+          message
+        })
+      });
+
+      if (response.ok) {
+        toast.success(`Email sent successfully!`, {
+          position: 'top-right',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'light',
+        });
+
+        setEmail('');
+        setSubject('');
+        setMessage('');
+      } else {
+        toast.error(`Failed to send email. Please try again later.`, {
+          position: 'top-right',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'light',
+        });
+      }
+    } catch (error) {
+      console.error('Error sending email:', error);
+      toast.error(`Failed to send email. Please try again later.`, {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+      });
+    }
+  };
+
   return (
     <section className="flex flex-wrap md:flex-nowrap min-h-screen">
       {/* Right side - Contact Form */}
@@ -37,29 +101,29 @@ const ContactPage = () => {
           <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 sm:text-xl">
             Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.
           </p>
-          <form action="#" className="space-y-8">
+          <form onSubmit={handleSubmit} className="space-y-8">
             <div>
               <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-              <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@flowbite.com" required />
+              <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@flowbite.com" required />
             </div>
             <div>
               <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900">Subject</label>
-              <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500" placeholder="Let us know how we can help you" required />
+              <input type="text" id="subject" value={subject} onChange={(e) => setSubject(e.target.value)} className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500" placeholder="Let us know how we can help you" required />
             </div>
             <div className="sm:col-span-2">
               <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">Your message</label>
-              <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Leave a comment..." required></textarea>
+              <textarea id="message" value={message} onChange={(e) => setMessage(e.target.value)} rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Leave a comment..." required></textarea>
             </div>
             <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-black hover:bg-black-500 focus:ring-4 focus:outline-none focus:ring-blue-300 w-full transition duration-300 ease-in-out">
               Send message
             </button>
-
           </form>
         </div>
         <footer className="text-center text-gray-500 text-sm p-4">
           © Copyright 2024. All rights reserved
         </footer>
 
+        {/* Social media icons */}
         <div className="flex justify-center space-x-4 mt-8">
           <a target='_blank' href="https://www.linkedin.com/in/richard-danon-woedey-5835a0292/" className="opacity-10 hover:opacity-100 duration-300 ease-in-out">{linkdnIconSVG()}</a>
           <a target='_blank' href="https://github.com/RichardDanon" className="opacity-10 hover:opacity-100 duration-300 ease-in-out">{gitHubIconSVG()}</a>
@@ -71,6 +135,9 @@ const ContactPage = () => {
       <div className="hidden md:block md:w-1/2">
         <img src={OfficeImage} alt="Office" className="h-screen w-full object-cover" />
       </div>
+
+      {/* Toast container */}
+      <ToastContainer />
     </section>
   );
 };
